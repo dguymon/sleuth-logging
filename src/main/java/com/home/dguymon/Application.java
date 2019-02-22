a009f48336424858a943c93c8de75a49b59b7c8e
Danny Guymon <VTdguymon@gmail.com>
2019-02-21 22:28:24 -0500

    Configure Eureka Client

--- src/main/java/com/home/dguymon/Application.java
+++ src/main/java/com/home/dguymon/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAutoConfiguration
+@EnableEurekaClient
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
